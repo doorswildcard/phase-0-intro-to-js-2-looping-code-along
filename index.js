316f931c948cf1bc81ea243597488e75a6758280
Ilan Rojas <bobatthevelvetroom@gmail.com>
2023-03-12 12:29:08 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    const newArray = [];
+    for (let index = 0; index < array.length; index++) {
+        newArray.push(`Thank you, ${array[index]}, for the wonderful surprise gift!`)
+
+    }
+    return newArray
+}
+
+function countDown(){
+    for (let index = 10; index >= 0; index--) {
+        console.log(index)
+
+    }
+}
+
+function countUp(){
+    for (let index = 0; index <= 10; index++) {
+        console.log(index)
+
+    }
+}
